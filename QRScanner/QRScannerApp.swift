7f52033ebad24ad8b48c2be54187824aa7d85d01
Omar <omarenriquelealr@gmail.com>
2025-03-30 09:13:08 -0400

    Initial Commit

---
+++ QRScanner/QRScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  QRScannerApp.swift
+//  QRScanner
+//
+//  Created by Omar Leal on 3/30/25.
+//
+
+import SwiftUI
+
+@main
+struct QRScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
